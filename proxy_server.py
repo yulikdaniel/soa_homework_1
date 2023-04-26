f5f2bf67e02d842a051457845222c2dc8d3fc699
Yuliy Daniel <yulik.daniel@gmail.com>
2023-04-26 16:38:15 +0200

    Added multicast

--- proxy_server.py
+++ proxy_server.py
@@ -1,10 +1,12 @@
 import socket
 import os
 import logging
+import struct
 
 logging.basicConfig(level=logging.INFO)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, struct.pack('b', 1))
 sock.bind(("0.0.0.0", int(os.getenv("PROXY_PORT"))))
 
 logging.info(f'Started listening on {("0.0.0.0", int(os.getenv("PROXY_PORT")))}')
@@ -14,9 +16,9 @@ while True:
     tester = tester.decode()
     logging.info(f"Received {tester} from {query_author}")
     if tester == "ALL":
-        for tester in os.getenv("BROADCAST_LIST", "NATIVE").split(","):
-            logging.info(f'Sending message to {(os.getenv(tester + "_HOST"), int(os.getenv(tester + "_PORT")))}')
-            sock.sendto(b"RUN", (os.getenv(tester + "_HOST"), int(os.getenv(tester + "_PORT"))))
+        logging.info(f'Sending message to {(os.getenv("MULTICAST_HOST"), int(os.getenv("MULTICAST_PORT")))}')
+        sock.sendto(b"RUN", (os.getenv("MULTICAST_HOST"), int(os.getenv("MULTICAST_PORT"))))
+        for _ in range(int(os.getenv("NUM_TESTERS", 7))):
             data, addr = sock.recvfrom(1024)
             logging.info("Redirecting response")
             sock.sendto(data, query_author)
--- testers/tester_server.py
+++ testers/tester_server.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import socket
+import struct
 import logging
 
 if __name__ == "__main__":
@@ -27,6 +28,11 @@ if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", int(os.getenv("PORT", "2001"))))
     logging.info(f'Started listening on {("0.0.0.0", int(os.getenv("PORT", "2001")))}')
+
+    logging.info(f'Registered for multicast on host {os.getenv("MULTICAST_HOST", "224.3.29.71")}')
+    group = socket.inet_aton(os.getenv("MULTICAST_HOST", "224.3.29.71"))
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     while True:
         data, addr = sock.recvfrom(1024)
